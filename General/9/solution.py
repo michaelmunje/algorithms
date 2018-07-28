5a6c21cea0f5369ccc31f5046d25470f3de9d7ef
Michael Munje <michael.munje.919@my.csun.edu>
2018-07-28 15:21:17 -0400

    Added better solution for general 9.

--- General/9/solution.py
+++ General/9/solution.py
@@ -8,9 +8,10 @@
 # function printMaxCount
 # Takes an array of integers as the input
 # Outputs the element that is repeated the most
+# takes 0.6 s for 5000
 def printPrimesBruteForce(numOfPrimes):
 	primes = list()
-	for i in range(2, numOfPrimes):
+	for i in range(2, numOfPrimes + 1):
 		isPrime = True
 		for j in range(2, i):
 			if (i % j == 0):
@@ -18,7 +19,23 @@ def printPrimesBruteForce(numOfPrimes):
 		if (isPrime == True):
 			primes.insert(len(primes),i)
 	print(primes)
+	print(len(primes))
+
+# takes 0.0 s for 5000
+def printPrimesBetter(numOfPrimes):
+	primes = [True] * (numOfPrimes + 1)
+	for i in range(2, numOfPrimes + 1):
+		for j in range(2, ((numOfPrimes + 1) // i) + 1):
+				if (i * j <= 5000):
+					primes[i * j] = False
+	primesList = list()
+	for i in range(2, numOfPrimes + 1):
+		if (primes[i]):
+			primesList.insert(len(primes),i)
+	print(primesList)
+	print(len(primesList))
 
 if __name__ == '__main__':
-	x = 30
+	x = 5000
 	printPrimesBruteForce(x)
+	printPrimesBetter(x)
