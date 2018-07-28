22b339d7293b11465a47f56adca7a65162beabbb
Michael Munje <michael.munje.919@my.csun.edu>
2018-07-27 21:19:30 -0400

    Added documentation for solution.

--- General/1/solution.py
+++ General/1/solution.py
@@ -1,6 +1,14 @@
+# General Problem 1:
+# Find the most frequent integer in an array
+
+# Solution: Store the array into a dictionary while counting the occurences
+# Store the array into a dictionary while counting the occurences
+# Then print out max value in dictionary
+
+# function printMaxCount
 # Takes an array of integers as the input
 # Outputs the element that is repeated the most
-def main(arr):
+def printMaxCount(arr):
 	myDict = dict()
 	for i in arr:
 		myDict[i] = myDict.get(i,0) + 1
@@ -9,4 +17,4 @@ def main(arr):
 
 if __name__ == '__main__':
 	arr = [8, 5, 9, 5, 2, 2, 2, 5, 5]
-	main(arr)
+	printMaxCount(arr)
