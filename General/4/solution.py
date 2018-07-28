cb7bddee9bfd511f2b8640db16eb5eac109d399f
Michael Munje <michael.munje.919@my.csun.edu>
2018-07-28 18:12:55 -0400

    Added recursive solution for general 4.

--- General/4/solution.py
+++ General/4/solution.py
@@ -5,31 +5,38 @@
 # Add the basis 1, and 1.
 # Then print out max value in dictionary
 
-# function listFibonacciDynamic
+# function getFibonacciDynamic
 # Takes the number of fibonacci
-# Outputs the sequence where number of integers is equal to input
+# Outputs the fib. sequence where the length is equal to input
 
-def listFibonacciRecursive(numOfFib):
-	fibonacci = list()
+def getFibonacciRecursive(fibIndex,fibonacciRec):
+	if (fibIndex > 1):
+		currentSum = getFibonacciRecursive(fibIndex - 1, fibonacciRec) + getFibonacciRecursive(fibIndex - 2, fibonacciRec)
+	else:
+		currentSum = 1
+	if (fibIndex >= len(fibonacciRec)):
+		fibonacciRec.insert(fibIndex, currentSum)
+	print(fibIndex)
+	return currentSum
+
+def putFibBasis(fibonacciRec):
+	fibonacciRec.insert(0,1)
+	fibonacciRec.insert(1,1)
+
+def getFibonacciDynamic(numOfFib,fibonacci):
 	if (numOfFib >= 1):
 		fibonacci.insert(0,1)
 	if (numOfFib >= 2):
 		fibonacci.insert(1,1)
 	for i in range(0, numOfFib):
 			fibonacci.insert(i + 2, fibonacci[i] + fibonacci[i + 1])
-	return fibonacci
-
-def listFibonacciDynamic(numOfFib):
-	fibonacci = list()
-	if (numOfFib >= 1):
-		fibonacci.insert(0,1)
-	if (numOfFib >= 2):
-		fibonacci.insert(1,1)
-	for i in range(0, numOfFib):
-			fibonacci.insert(i + 2, fibonacci[i] + fibonacci[i + 1])
-	return fibonacci
 
 if __name__ == '__main__':
-	x = 50
-	fibonacci = listFibonacci(x)
+	x = 10
+	fibonacci = list()
+	fibonacciRec = list()
+	getFibonacciDynamic(x,fibonacci)
 	print(fibonacci)
+	putFibBasis(fibonacciRec)
+	getFibonacciRecursive(x - 1,fibonacciRec)
+	print(fibonacciRec)
