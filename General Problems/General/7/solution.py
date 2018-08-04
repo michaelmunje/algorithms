a16203b8df624ea7be23e9f4ef387697c9feab51
Michael Munje <michael.munje.919@my.csun.edu>
2018-08-03 22:01:22 -0400

    Added recursive solution and comments.

--- General Problems/General/7/solution.py
+++ General Problems/General/7/solution.py
@@ -2,8 +2,32 @@
 # Implement binary search of a sorted array of integers
 
 # Solution:
+# Binary search algorithm
+# 3 functions:
+# recursion, specifically divide and conquer recursion
+# while loop that uses divide and conquer
+# while loop that doesn't use start and end, uses divide and conquer
 
 import math
+
+def binarySearchRec(arr, start, end, value):
+	if (start <= end):
+		mp = (start + end) // 2
+		if (arr[mp] == value):
+			return mp
+		elif (arr[mp] < value):
+			getBin = binarySearchRec(arr, mp + 1, end, value)
+			if (getBin != -1):
+				return getBin
+			else:
+				return -1
+		else:
+			getBin = binarySearchRec(arr, start, mp - 1, value)
+			if (getBin != -1):
+				return getBin
+			else:
+				return -1
+	return -1
 
 def binarySearchEasy(arr, value):
 	end = len(arr) - 1
@@ -45,8 +69,9 @@ def binarySearchComplicated(arr, value):
 
 if __name__ == '__main__':
 	arr = [0, 1, 3, 4, 5, 6, 7, 8, 9, 10]
-	val = 9
-	index = binarySearchComplicated(arr, val)
+	val = 3
+	#index = binarySearchComplicated(arr, val)
+	index = binarySearchRec(arr, 0, len(arr)-1, val)
 	if (index != -1):
 		print(index)
 	else:
