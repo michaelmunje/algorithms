741e79cc0d6bd85dbdbb74bb2e360e452dee9a40
Michael Munje <michael.munje.919@my.csun.edu>
2018-08-03 23:44:44 -0400

    Added solution and comments for general 10.

--- General Problems/General/10/solution.py
+++ General Problems/General/10/solution.py
@@ -2,5 +2,25 @@
 # Write a function that prints out the binary form of an int
 
 # Solution:
+# Divide by 2 and see if the current number is a multiple of 2
+# Repeat until the num is equal to 0
+# Reverse the string and return it
+
+import math
+
+def intToBin(inputInt):
+	outputString = ""
+	currentNum = inputInt
+	for i in range(0, int(math.log(inputInt, 2)) + 1):
+		if (currentNum % 2 == 0):
+			outputString = outputString + "0"
+		else:
+			outputString = outputString + "1"
+		currentNum = currentNum // 2
+	outputString = outputString[::-1]
+	return outputString
 
 if __name__ == '__main__':
+	inputInt = 4096
+	outputString = intToBin(inputInt)
+	print(outputString)
