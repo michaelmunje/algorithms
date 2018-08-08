74f521cdc5be88d669fd72ac655fa0f950bf672a
Michael Munje <michael.munje.919@my.csun.edu>
2018-08-07 23:59:25 -0400

    Added brute force solution for general 13.

--- General Problems/General/13/solution.py
+++ General Problems/General/13/solution.py
@@ -3,4 +3,24 @@
 
 # Solution:
 
+def getExponentBrute(x, y):
+	exp = 1
+	for i in range(1, y + 1):
+		exp *= x
+	return exp
+
+def getExponentFast(x, y):
+	exp = 1
+	if (y % 2 == 1):
+		exp *= x
+	y2 = y - (y % 2)
+	while (y2 > 1):
+		y2 = y2 // 2
+		if (y2 )
+	return exp
+
+
 if __name__ == '__main__':
+	x = 2
+	y = 6
+	print(getExponentFast(x,y))
