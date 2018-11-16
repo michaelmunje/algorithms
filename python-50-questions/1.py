1344e26234bb9cbee41d0fc381adb2f20f3482ff
Michael Munje <michael.munje.919@my.csun.edu>
2018-11-16 13:30:46 -0800

    Added base file for problem 1.

---
+++ python-50-questions/1.py
@@ -0,0 +1,21 @@
+# ✅ Ryerson letter grade
+
+# def ryerson_letter_grade(pct):
+
+# Given the percentage grade for the course, calculate and return the letter grade that would appear in the Ryerson grades transcript, as defined on the page Ryerson Grade Scales. The letter grade should be returned as a string that consists of the uppercase letter followed by the possible modifier "+" or "-". The function should work correctly for all values of pct from 0 to 150.
+
+# (Same as all 49 programming problems that follow this problem, this can be solved in various different ways.)
+
+# pct	Expected result
+# 45	"F"
+# 62	"C-"
+# 89	"A"
+# 107	"A+"
+
+
+def ryerson_letter_grade(pct):
+    return "F"
+
+
+if __name__ == '__main__':
+    ryerson_letter_grade(45)
