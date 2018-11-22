2367215b30655a784f88b624541b67ea4da733f9
Michael Munje <michael.munje.919@my.csun.edu>
2018-11-21 22:34:22 -0800

    Added base file for 1.9

---
+++ cracking-the-coding-interview/1-9.py
@@ -0,0 +1,13 @@
+# String Rotation: Assume you have a method isSubst ring which checks if one word is a substring
+# of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one
+# call to isSubstring (e.g., "waterbottle" is a rotation of"erbottlewat").
+
+
+def isRotation(s1, s2):
+    
+
+
+def testRotations():
+   
+def testNoRotations():
+     assert ()
