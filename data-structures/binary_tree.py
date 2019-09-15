725134b52aa45c8fb1832843ac83c69bc5ad52ef
Michael Munje <michaelmunje@gmail.com>
2019-09-15 13:29:20 -0700

    Add WIP implementation for heap

--- data-structures/binary_tree.py
+++ data-structures/binary_tree.py
@@ -1,4 +1,5 @@
 import unittest
+from stack import Stack
 
 
 class Node:  # Binary Node
@@ -17,6 +18,47 @@ def binary_search(head, value):  # Returns true if value exists
         return binary_search(head.left, value)
     else:
         return binary_search(head.right, value)
+
+
+class Heap:  # Max heap, min heap can be obtained by multipying values by -1
+    def __init__(self):
+        self.head = None
+
+    def add_node(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return
+        current_node = self.head
+        stack = Stack()
+        while current_node:
+            stack.push(current_node)
+            current_node = current_node.right
+        current_node = Node(value)
+        parent_node = stack.pop()
+        while parent_node:
+            if value > parent_node.value:
+                tmp = parent_node.left
+                parent_node.left = current_node.left
+                current_node.left = tmp
+                parent_node.right = current_node.right
+                current_node.right = parent_node
+                if not stack.is_empty():
+                    parent_node = stack.pop()
+                else:
+                    parent_node = None
+            else:
+                break
+
+    def tree_string(self, current_node):
+        if not current_node:
+            return ''
+        else:
+            return str(current_node.value) + '\n' \
+                   + '(' + self.tree_string(current_node.left) + ')' \
+                   + '(' + self.tree_string(current_node.right) + ')'
+
+    def __repr__(self):
+        return self.tree_string(self.head)
 
 
 class TestSolution(unittest.TestCase):
@@ -39,6 +81,13 @@ class TestSolution(unittest.TestCase):
     def test_case_2(self):
         head = None
         self.assertFalse(binary_search(head, 1))
+
+    def test_case_3(self):
+        heap = Heap()
+        heap.add_node(5)
+        heap.add_node(6)
+        heap.add_node(7)
+        print(heap)
 
 
 if __name__ == '__main__':
