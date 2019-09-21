a170edb70d9cc1a7f7dc1d2cb8b06bcaf738825e
Michael Munje <michaelmunje@gmail.com>
2019-09-21 12:25:55 -0700

    Add shortest path algorithm

--- data-structures/graph.py
+++ data-structures/graph.py
@@ -1,24 +1,20 @@
 import unittest
 from queue import Queue
+from priority_queue import PQueue
+import sys
 
 
 def dfs(g, s, d):
     visited = [False] * len(g.nodes)
-    current_node = s
-    if depth_first_search(g, s, d, visited) is True:
-        return True
-    else:
-        return False
+    depth_first_search(g, s, d, visited)
+    return visited[d]
 
 
 def depth_first_search(g, s, d, visited):
     visited[s] = True
     for i in range(len(g.nodes)):
         if g.edges[s][i] == 1 and visited[i] is False:
-            if d == i:
-                return True
-            else:
-                return depth_first_search(g, i, d, visited)
+            depth_first_search(g, i, d, visited)
 
 
 def breadth_first_search(g, s, d):
@@ -37,17 +33,49 @@ def breadth_first_search(g, s, d):
     return False
 
 
-class UnweightedGraph:
+def shortest_path(g, s, d):
+    dist = [sys.maxsize] * len(g.nodes)
+    prev = [None] * len(g.nodes)
+    dist[s] = 0
+
+    visited = [False] * len(g.nodes)
+    pq = PQueue()
+    pq.enqueue(s, 0)
+
+    while not pq.is_empty():
+        current, weight = pq.dequeue()
+        visited[current] = True
+        for i in range(len(g.nodes)):
+            if g.edges[current][i] > 0:
+                current_dist = g.edges[current][i] + dist[current]
+
+                if current_dist < dist[i]:
+                    dist[i] = current_dist
+                    prev[i] = current
+
+                if not visited[i]:
+                    pq.enqueue(i, current_dist)
+
+    return dist[d]
+
+
+class Graph:
     def __init__(self, size):
-        self.edges = [[0] * size] * size
+        self.edges = [0] * size
+        for i in range(size):
+            self.edges[i] = [0] * size
+
         self.nodes = [''] * size
 
-    def add_undirected_edge(self, s, d):
-        self.add_directed_edge(s, d)
-        self.add_directed_edge(d, s)
+    def add_undirected_edge(self, s, d, m):
+        self.add_directed_edge(s, d, m)
+        self.add_directed_edge(d, s, m)
 
-    def add_directed_edge(self, s, d):
+    def add_directed_edge_unweighted(self, s, d):
         self.edges[s][d] = 1
+
+    def add_directed_edge(self, s, d, mag):
+        self.edges[s][d] = mag
 
     def remove_undirected_edge(self, s, d):
         self.remove_directed_edge(s, d)
@@ -66,30 +94,53 @@ class UnweightedGraph:
 class TestSolution(unittest.TestCase):
 
     def test_case1(self):
-        g = UnweightedGraph(6)
-        g.add_directed_edge(0, 1)
-        g.add_directed_edge(1, 2)
-        g.add_directed_edge(1, 3)
+        g = Graph(6)
+        g.add_directed_edge_unweighted(0, 1)
+        g.add_directed_edge_unweighted(1, 2)
+        g.add_directed_edge_unweighted(1, 3)
         self.assertTrue(dfs(g, 0, 1))
         self.assertTrue(dfs(g, 0, 3))
         self.assertFalse(dfs(g, 0, 4))
-        g.add_directed_edge(4, 5)
-        g.add_directed_edge(3, 4)
+        g.add_directed_edge_unweighted(4, 5)
+        g.add_directed_edge_unweighted(3, 4)
         self.assertTrue(dfs(g, 0, 4))
         self.assertTrue(dfs(g, 0, 5))
 
     def test_case2(self):
-        g = UnweightedGraph(6)
-        g.add_directed_edge(0, 1)
-        g.add_directed_edge(1, 2)
-        g.add_directed_edge(1, 3)
+        g = Graph(6)
+        g.add_directed_edge_unweighted(0, 1)
+        g.add_directed_edge_unweighted(1, 2)
+        g.add_directed_edge_unweighted(1, 3)
         self.assertTrue(breadth_first_search(g, 0, 1))
         self.assertTrue(breadth_first_search(g, 0, 3))
         self.assertFalse(breadth_first_search(g, 0, 4))
-        g.add_directed_edge(4, 5)
-        g.add_directed_edge(3, 4)
+        g.add_directed_edge_unweighted(4, 5)
+        g.add_directed_edge_unweighted(3, 4)
         self.assertTrue(breadth_first_search(g, 0, 4))
         self.assertTrue(breadth_first_search(g, 0, 5))
+
+    def test_case3(self):
+        g = Graph(6)
+        g.add_directed_edge(0, 1, 2)
+        g.add_directed_edge(0, 2, 7)
+        g.add_directed_edge(1, 3, 2)
+        g.add_directed_edge(1, 3, 2)
+        g.add_directed_edge(1, 2, 3)
+        g.add_directed_edge(2, 4, 1)
+        g.add_directed_edge(4, 5, 1)
+        g.add_directed_edge(2, 3, 5)
+        g.add_directed_edge(3, 5, 6)
+        self.assertEqual(shortest_path(g, 0, 5), 7)
+
+    def test_case4(self):
+        g = Graph(4)
+        g.add_directed_edge(0, 1, 1)
+        g.add_directed_edge(0, 2, 7)
+        g.add_directed_edge(1, 2, 1)
+        g.add_directed_edge(2, 0, 2)
+        g.add_directed_edge(2, 3, 1)
+        g.add_directed_edge(1, 3, 8)
+        self.assertEqual(shortest_path(g, 0, 3), 3)
 
 
 if __name__ == '__main__':
