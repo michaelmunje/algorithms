2d4ae1e456a2fff1ea347b9a4de67abb9b59f03d
Michael Munje <michaelmunje@gmail.com>
2019-09-20 21:16:03 -0700

    Finish implementation for heap.

--- data-structures/binary_tree.py
+++ data-structures/binary_tree.py
@@ -20,34 +20,50 @@ def binary_search(head, value):  # Returns true if value exists
         return binary_search(head.right, value)
 
 
-class Heap:  # Max heap, min heap can be obtained by multipying values by -1
+class Heap:  # Max heap, min heap can be obtained trivially
     def __init__(self):
-        self.head = None
+        self.values = []
+
+    def remove_max(self):
+        old_max = self.values[0]
+        if len(self.values) > 1:
+            self.values[0] = self.values[-1]
+            self.values.pop()
+
+        i = 0
+
+        while 2*i+1 < len(self.values):
+
+            swap_i = 2*i+1
+
+            if 2*i + 2 < len(self.values):
+                if max(self.values[2*i+1], self.values[2*i+2]) <= self.values[i]:
+                    break
+
+                if self.values[2*i+2] > self.values[2*i+1]:
+                    swap_i += 1
+            else:
+                if self.values[2*i+1] <= self.values[i]:
+                    break
+
+            tmp = self.values[i]
+            self.values[i] = self.values[swap_i]
+            self.values[swap_i] = tmp
+            i = swap_i
+
+        return old_max
 
     def add_node(self, value):
-        if self.head is None:
-            self.head = Node(value)
+        self.values.append(value)
+        current_i = len(self.values) - 1
+        if current_i == 0:
             return
-        current_node = self.head
-        stack = Stack()
-        while current_node:
-            stack.push(current_node)
-            current_node = current_node.right
-        current_node = Node(value)
-        parent_node = stack.pop()
-        while parent_node:
-            if value > parent_node.value:
-                tmp = parent_node.left
-                parent_node.left = current_node.left
-                current_node.left = tmp
-                parent_node.right = current_node.right
-                current_node.right = parent_node
-                if not stack.is_empty():
-                    parent_node = stack.pop()
-                else:
-                    parent_node = None
-            else:
-                break
+
+        while self.values[current_i] > self.values[current_i - 1 // 2]:
+            tmp = self.values[current_i]
+            self.values[current_i] = self.values[current_i - 1 // 2]
+            self.values[current_i - 1 // 2] = tmp
+            current_i = current_i - 1 // 2
 
     def tree_string(self, current_node):
         if not current_node:
@@ -58,7 +74,7 @@ class Heap:  # Max heap, min heap can be obtained by multipying values by -1
                    + '(' + self.tree_string(current_node.right) + ')'
 
     def __repr__(self):
-        return self.tree_string(self.head)
+        return str(self.values) #self.tree_string(self.head)
 
 
 class TestSolution(unittest.TestCase):
@@ -84,10 +100,30 @@ class TestSolution(unittest.TestCase):
 
     def test_case_3(self):
         heap = Heap()
+        heap.add_node(25)
+        heap.add_node(13)
+        heap.add_node(12)
+        heap.add_node(31)
+        heap.remove_max()
+        heap.remove_max()
+        self.assertTrue(str(heap) == '[13, 12]')
+
+    def test_case_4(self):
+        heap = Heap()
+        heap.add_node(31)
+        heap.add_node(15)
+        heap.add_node(17)
+        self.assertTrue(str(heap) == '[31, 15, 17]')
+        heap.add_node(2)
+        heap.add_node(3)
         heap.add_node(5)
-        heap.add_node(6)
-        heap.add_node(7)
-        print(heap)
+        self.assertTrue(str(heap) == '[31, 15, 17, 2, 3, 5]')
+        heap.remove_max()
+        self.assertTrue(str(heap) == '[17, 15, 5, 2, 3]')
+        heap.remove_max()
+        self.assertTrue(str(heap) == '[15, 3, 5, 2]')
+        heap.remove_max()
+        self.assertTrue(str(heap) == '[5, 3, 2]')
 
 
 if __name__ == '__main__':
