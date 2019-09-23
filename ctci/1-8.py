6f45c1c3d6082cb4e303a0b74f5404974a659616
Michael Munje <michaelmunje@gmail.com>
2019-09-22 21:13:24 -0700

    Optimize solution a little more.

--- ctci/1-8.py
+++ ctci/1-8.py
@@ -23,26 +23,27 @@ def set_zeroes(matrix):
 
 def set_zeroes_optimized(matrix):
     is_col_zero = False
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
+    r = len(matrix)
+    c = len(matrix[0])
+    for i in range(r):
+        if matrix[i][0] == 0:
+            is_col_zero = True
+        for j in range(1, c):
             if matrix[i][j] == 0:
                 matrix[i][0] = 0
-                if j == 0:
-                    is_col_zero = True
-                else:
-                    matrix[0][j] = 0
+                matrix[0][j] = 0
 
-    for i in range(1, len(matrix)):
-        for j in range(1, len(matrix[0])):
+    for i in range(1, r):
+        for j in range(1, c):
             if matrix[i][0] == 0 or matrix[0][j] == 0:
                 matrix[i][j] = 0
 
     if matrix[0][0] == 0:
-        for j in range(len(matrix[0])):
+        for j in range(c):
             matrix[0][j] = 0
 
     if is_col_zero:
-        for i in range(len(matrix)):
+        for i in range(r):
             matrix[i][0] = 0
 
     return matrix
