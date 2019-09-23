d3a632f3229f1065561705982a6e359ff79fde0a
Michael Munje <michaelmunje@gmail.com>
2019-09-22 21:33:51 -0700

    Add solutions for checking string rotation

--- ctci/1-9.py
+++ ctci/1-9.py
@@ -1,13 +1,40 @@
-# String Rotation: Assume you have a method isSubst ring which checks if one word is a substring
-# of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one
-# call to isSubstring (e.g., "waterbottle" is a rotation of"erbottlewat").
+import unittest
 
 
-def isRotation(s1, s2):
-    
+def check_rotation(a, b):
+    return len(a) == len(b) and b in a + a
 
 
-def testRotations():
-   
-def testNoRotations():
-     assert ()
+def check_rotation_2(A, B):
+    if len(A) != len(B):
+        return False
+
+    if A == B:
+        return True
+
+    for i in range(len(A)):
+        if A[i:] + A[:i] == B:
+            return True
+    return False
+
+
+class TestSolution(unittest.TestCase):
+    def test_rotation(self):
+        self.assertTrue(check_rotation('waterbottle', 'erbottlewat'))
+        self.assertTrue(check_rotation('car', 'rca'))
+
+    def test_no_rotation(self):
+        self.assertFalse(check_rotation('waterbottler', 'erbottlewata'))
+        self.assertFalse(check_rotation('cars', 'rca'))
+
+    def test_rotation_2(self):
+        self.assertTrue(check_rotation_2('waterbottle', 'erbottlewat'))
+        self.assertTrue(check_rotation_2('car', 'rca'))
+
+    def test_no_rotation_2(self):
+        self.assertFalse(check_rotation_2('waterbottler', 'erbottlewata'))
+        self.assertFalse(check_rotation_2('cars', 'rca'))
+
+
+if __name__ == '__main__':
+    unittest.main()
