12884949302a2eb497d7d808c1859d6d7e9503fa
Michael Munje <michaelmunje@gmail.com>
2019-09-22 21:05:50 -0700

    Add optimized solution for setting zeroes.

--- ctci/1-8.py
+++ ctci/1-8.py
@@ -21,9 +21,43 @@ def set_zeroes(matrix):
     return matrix
 
 
+def set_zeroes_optimized(matrix):
+    is_col_zero = False
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                if j == 0:
+                    is_col_zero = True
+                else:
+                    matrix[0][j] = 0
+
+    for i in range(1, len(matrix)):
+        for j in range(1, len(matrix[0])):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+
+    if matrix[0][0] == 0:
+        for j in range(len(matrix[0])):
+            matrix[0][j] = 0
+
+    if is_col_zero:
+        for i in range(len(matrix)):
+            matrix[i][0] = 0
+
+    return matrix
+
+
 class TestSolution(unittest.TestCase):
     def test_set_zeroes(self):
         self.assertEqual(set_zeroes([[1,1,1],[1,0,1],[1,1,1]]), [[1,0,1],[0,0,0],[1,0,1]])
+        self.assertEqual(set_zeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]]), [[0,0,0,0],[0,4,5,0],[0,3,1,0]])
+        self.assertEqual(set_zeroes([[1,1,1],[0,1,2]]), [[0,1,1],[0,0,0]])
+
+    def test_set_zeroes_opt(self):
+        self.assertEqual(set_zeroes_optimized([[1,1,1],[1,0,1],[1,1,1]]), [[1,0,1],[0,0,0],[1,0,1]])
+        self.assertEqual(set_zeroes_optimized([[0,1,2,0],[3,4,5,2],[1,3,1,5]]), [[0,0,0,0],[0,4,5,0],[0,3,1,0]])
+        self.assertEqual(set_zeroes_optimized([[1,1,1],[0,1,2]]), [[0,1,1],[0,0,0]])
 
 
 if __name__ == '__main__':
