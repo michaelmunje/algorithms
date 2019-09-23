1f551b752b705a11cdcc64745fde2c9fdc2b8fb4
Michael Munje <michaelmunje@gmail.com>
2019-09-23 14:35:15 -0700

    Add solution for forward sum.

--- ctci/ch2/2-5.py
+++ ctci/ch2/2-5.py
@@ -64,8 +64,50 @@ def add_linked_lists_backwards(head1, head2):  # O(n)
     return sum_head
 
 
+def add_linked_lists_forwards(l1, l2):  # O(n)
+    s1 = []
+    s2 = []
+    current_node = l1
+    while current_node:
+        s1.append(current_node.val)
+        current_node = current_node.next
+    current_node = l2
+    while current_node:
+        s2.append(current_node.val)
+        current_node = current_node.next
+
+    carry_over = 0
+    total_sum = []
+
+    for i in range(max(len(s1), len(s2))):
+
+        if i < len(s1) and i < len(s2):
+            new_sum = s1[-1 - i] + s2[-1 - i] + carry_over
+        elif i < len(s1):
+            new_sum = s1[-1 - i] + carry_over
+        else:
+            new_sum = s2[-1 - i] + carry_over
+
+        total_sum.append(new_sum % 10)
+
+        if new_sum >= 10:
+            carry_over = 1
+        else:
+            carry_over = 0
+
+    if carry_over == 1:
+        total_sum.append(1)
+
+    head = Node(total_sum[-1])
+    current_node = head
+    for i in range(1, len(total_sum)):
+        current_node.next = Node(total_sum[-1 - i])
+        current_node = current_node.next
+    return head
+
+
 class TestSolution(unittest.TestCase):
-    def test_node_removal(self):
+    def test_adding_backwards(self):
         head = list_to_linked_list([7, 1, 6])
         head2 = list_to_linked_list([5, 9, 2])
         self.assertEqual(linked_list_list(add_linked_lists_backwards(head, head2)), [2, 1, 9])
@@ -75,6 +117,17 @@ class TestSolution(unittest.TestCase):
         head = list_to_linked_list([7, 1, 9])
         head2 = list_to_linked_list([])
         self.assertEqual(linked_list_list(add_linked_lists_backwards(head, head2)), [7, 1, 9])
+
+    def test_adding_forwards(self):
+        head = list_to_linked_list([7, 1, 6])
+        head2 = list_to_linked_list([5, 9, 2])
+        self.assertEqual(linked_list_list(add_linked_lists_forwards(head, head2)), [1, 3, 0, 8])
+        head = list_to_linked_list([7, 1, 9])
+        head2 = list_to_linked_list([5, 9, 1])
+        self.assertEqual(linked_list_list(add_linked_lists_forwards(head, head2)), [1, 3, 1, 0])
+        head = list_to_linked_list([7, 1, 9])
+        head2 = list_to_linked_list([])
+        self.assertEqual(linked_list_list(add_linked_lists_forwards(head, head2)), [7, 1, 9])
 
 
 if __name__ == '__main__':
