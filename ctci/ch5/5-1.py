178cc1a4c1688a079b6f4d640e18083be6c417b1
Michael Munje <michaelmunje@gmail.com>
2019-09-29 02:46:06 -0700

    Add masking to solution.

--- ctci/ch5/5-1.py
+++ ctci/ch5/5-1.py
@@ -2,19 +2,26 @@ import unittest
 
 
 def bit_insertion(a, b, i, j):
-
-    return (a >> j << j) + (b << i)
+    mask = ~0
+    mask <<= j
+    mask += (1 << i) - 1
+    return (a & mask) + (b << i)
 
 
 class TestSolution(unittest.TestCase):
     def test_stacks(self):
+
         a = 2048
         b = 19
         self.assertEqual(bit_insertion(a, b, 2, 6), 2124)
 
         a = 2057
         b = 19
-        self.assertEqual(bit_insertion(a, b, 2, 6), 2124)
+        self.assertEqual(bit_insertion(a, b, 2, 6), 2125)
+
+        a = 2049
+        b = 19
+        self.assertEqual(bit_insertion(a, b, 2, 6), 2125)
 
 
 if __name__ == '__main__':
